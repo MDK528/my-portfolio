b0ea92face9714f997eee1d8f87b1f3625abf374
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2025-08-02 02:02:04 +0530

    Update About.jsx
    update the about.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,16 +8,18 @@ function About()
             <section id='about'>
                 <div className='w-full px-8 py-5 text-white md:grid md:grid-cols-2 md:gap-20 md:place-content-center'>
                 <div className='px-5 py-3  flex justify-start'>
-                    <p className='text-[15px] md:text-[20px]'>
-                        <span className='text-2xl block font-bold mb-4'>About Me</span>
-                        Hi, I'm Md Khalid Hossain, a passionate and self-taught web developer with a strong interest in building clean and user-friendly websites. I enjoy turning complex problems into simple, beautiful, and intuitive designs.
+                    <div>
+                            <h2 className='text-2xl md:text-3xl font-bold mb-4'> About Me</h2>
+                            <p className='text-base md:text-xl'>
+                                I'm Md Khalid Hossain, a passionate software developer focused on building clean, user-friendly websites and responsive full-stack web applications. 
 
-                        Currently, I'm focusing on mastering React.js and building full-stack web applications using Node.js and MySQL. I also love working with Tailwind CSS to create responsive and elegant UIs.
+                                Currently, I'm focusing on mastering React.js and building full-stack web applications using Node.js and MySQL. I also love working with Tailwind CSS to create responsive and elegant UIs.
 
-                        I'm on a journey to become a full-stack developer, and I believe that strong fundamentals and consistent practice are the keys to mastering web development.
+                                I'm on a journey to become a full-stack developer, and I believe that strong fundamentals and consistent practice are the keys to mastering web development.
 
-                        When I'm not coding, I'm exploring the latest trends in technology.
-                    </p>
+                                When I'm not coding, I'm exploring the latest trends in technology.
+                            </p>
+                        </div>
                 </div>
 
                 <div className="px-5 py-5 flex justify-end">
